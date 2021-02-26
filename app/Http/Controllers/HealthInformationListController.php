598089027bea4e9b2dd3257c3009a5d88efa77d8
LuMingfu <lumingfu@fazheng.com.cn>
2021-02-26 10:56:05 +0800

    'update'

--- app/Http/Controllers/HealthInformationListController.php
+++ app/Http/Controllers/HealthInformationListController.php
@@ -10,10 +10,11 @@ class HealthInformationListController extends Controller {
     public function index() {
         $data = file_get_contents('php://input');
         $data = json_decode($data,true);
-        $list   = DB::select('select * from userweight where userId = :id', ['id' => $data['userId']]);
+        $list   = DB::select('select * from userweight where userId = :id ORDER BY id DESC LIMIT 0,7', ['id' => $data['userId']]);
 
         $result = $list;
-        $result = '{"code":"200","data":{'.$list.'},"msg":"success"}';
+        // $result = '{"code":"200","data":{'.$list.'},"msg":"success"}';
+        $result =  response()->json(['code' => '200', 'data' => $list , 'msg' => 'success']);
         // if($user && $user[0]->username && $data['password'] == $user[0]->password){
         //     $result = '{"code":"200","data":{"id":"'.$user[0]->id.'","phone":"'.$user[0]->phone.'","name":"'.$user[0]->name.'","nickname":"'.$user[0]->nickname.'"},"msg":"登录成功"}';
         // }else{
---
+++ border.md
@@ -0,0 +1,1 @@
+>php artisan serve --host=10.0.110.191 --port=8000

8b83a23d0700ed469e6153b7fa498da54d451def
lumingfu <hgsqzx@163.com>
2021-02-27 11:00:49 +0800

    'update'

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//  设置允许跨域
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Methods:GET,POST,PUT,DELETE');
+header('Access-Control-Allow-Headers:Origin, Content-Type, Cookie, Accept, X-CSRF-TOKEN');
+header('Access-Control-Allow-Credentials:true');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
